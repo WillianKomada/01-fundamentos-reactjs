dd2594f991221d3c3f66fb15c7f50838304e8367
Willian Komada <willian10komada@hotmail.com>
2023-08-03 16:38:07 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <>
+      <Post
+        author="Willian Komada"
+        content="Front-end Developer at Home Office"
+      />
+      <Post
+        author="Diego Fernandes"
+        content="Developer at Rocketseat"
+      />
+      <Post
+        author="Luccas Marchielo"
+        content="Developer at NASA"
+      />
+    </>
+  );
+}
