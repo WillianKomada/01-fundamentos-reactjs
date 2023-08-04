5dd4c1832c0868531b10889a699bd09e1e4b1046
Willian Komada <willian10komada@hotmail.com>
2023-08-04 19:41:44 -0300

    segundo dia de estudo e aplicando a sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post";
+
+import styles from "./App.module.css";
+import "./global.css";
 
 export function App() {
   return (
     <>
-      <Post
-        author="Willian Komada"
-        content="Front-end Developer at Home Office"
-      />
-      <Post
-        author="Diego Fernandes"
-        content="Developer at Rocketseat"
-      />
-      <Post
-        author="Luccas Marchielo"
-        content="Developer at NASA"
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+
+        <main>
+          <Post
+            author="Willian Komada"
+            content="Front-end Developer at Home Office"
+          />
+          <Post author="Diego Fernandes" content="Developer at Rocketseat" />
+          <Post author="Luccas Marchielo" content="Developer at NASA" />
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine } from "phosphor-react";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1636489953081-c4ebbd50fa3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/39250804?v=4"
+          alt="Avatar"
+        />
+        <strong>Willian Komada</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
